49521dc1029f546ba8122f5d26cea7c3919e9aea
gervyg <gervygonzalez@gmail.com>
2022-03-02 21:07:12 -0300

    repofbi

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const agentes = require('./data/agentes.js')
 const app = express()
-const axios = require('axios')
 const jwt = require('jsonwebtoken')
 
-const secretKey = 'clave'
+const secretKey = process.env["SECRET_KEY"];
+console.log(secretKey)
+
+app.use(express.static('public'));
 
 
 app.get('/', (req, res) => {
@@ -12,43 +14,49 @@ app.get('/', (req, res) => {
 });
 
 
-app.get('/SignIn', (req, res) => {   
-    const { email, password } = req.query;   
-    const user = (agentes.results).find((u) => u.email == email && u.password ==  password);
-  
-    if (user) {  
-    const token = jwt.sign( {exp: Math.floor(Date.now() / 1000) + 120, data: user, },
-    secretKey
-    );
- 
-    res.send(`<a href="/Dashboard?token=${token}"> <p> Ir al Dashboard </p> </a>
+app.get('/SignIn', (req, res) => {
+    const { email, password } = req.query;
+    const user = (agentes.results).find((u) => u.email == email && u.password == password);
+
+    if (user) {
+        const token = jwt.sign({ exp: Math.floor(Date.now() / 1000) + 120, data: user, },
+            secretKey
+        );
+
+        res.send(`<a href="/RutaRestringida?token=${token}"> <p> Ir a la RutaRestringida </p> </a>
     Bienvenido, ${email}.
     <script>
     localStorage.setItem('token', JSON.stringify("${token}"))
     </script>
     `);
-    } else {   
-    res.send("Usuario o contraseña incorrecta");
+    } else {
+        res.send("Usuario o contraseña incorrecta");
     }
-    });
+});
 
-    
-app.get("/Dashboard", (req, res) => {
- 
-    let { token } = req.query; 
-    jwt.verify(token, secretKey, (err, decoded) => {   
-    err
-    ? res.status(401).send({
-    error: "401 Unauthorized",
-    message: err.message,
+const Verificar = (req, res, next) => {
+    let { token } = req.query;
+    jwt.verify(token, secretKey, (err, decoded) => {
+        if (err) {
+            res.status(401).send({
+                error: "401 No Autorizado",
+                message: err.message,
+            })
+        } else {
+            req.user = decoded;
+            next();
+        };
+
+
     })
-    : 
-    res.send(` Bienvenido al Dashboard ${decoded.data.email}`);
-    });
-    });
+};
 
-  
 
-    app.listen(3000, () => console.log('Sevidor ON en puerto 3000'))
+app.get("/RutaRestringida", Verificar, (req, res) => {
+    res.send(` Bienvenido a la Zona X ${req.user.data.email}`);
+});
 
-    
+
+
+app.listen(3000, () => console.log('Sevidor ON en puerto 3000'))
+
